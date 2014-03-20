598fcd65da64f6068605d6b2ebd20c7c3934d89d
jasonsbrooks <jason.brooks@yale.edu>
2014-03-19 23:07:40 -0400

    Fixed default MAX id to be 0

--- reset.py
+++ reset.py
@@ -2,7 +2,7 @@ from app.models import *
 import datetime
 db.drop_all()
 db.create_all()
-u = User(name="Max", fbid="12345", privacy=0)
+u = User(id=0, name="Max", fbid="12345", privacy=0)
 db.session.add(u)
 db.session.commit()
 
