c22d1b1e25b99a352cd265c91ea55fc994e2fd41
jasonsbrooks <jason.brooks@yale.edu>
2014-03-19 23:09:59 -0400

    Commented reset script

--- reset.py
+++ reset.py
@@ -1,7 +1,10 @@
 from app.models import *
 import datetime
+print "Dropping Tables"
 db.drop_all()
+print "Creating Tables"
 db.create_all()
+print "Creating Default User"
 u = User(id=0, name="Max", fbid="12345", privacy=0)
 db.session.add(u)
 db.session.commit()
