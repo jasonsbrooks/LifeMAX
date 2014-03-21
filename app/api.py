4f28719f73785b9a8ad969b9d37c17d2d32b8095
Micah Rosales <micah.rosales@gmail.com>
2014-03-21 03:24:03 -0700

    Added abort mapping

--- app/api.py
+++ app/api.py
@@ -29,6 +29,12 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 true=True
 
+class ResourceNotFound(ex.HTTPException):
+    code = 404
+    description = '<p>resource not found</p>'
+
+abort.mappings[404] = ResourceNotFound
+
 def createTaskJSON(task):
 	u = models.User.query.get(task.user)
 	userJSON = {'id' : u.id, 'name' : u.name, 'fbid': u.fbid}
